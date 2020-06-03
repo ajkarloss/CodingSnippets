51964fdf1122a79999a4f633d06d6ef43744829d
Jeevan Karloss <ajkarloss@gmail.com>
2020-06-03 11:17:22 +0200

    Update Moving_Files.sh

--- Moving_Files.sh
+++ Moving_Files.sh
@@ -1,7 +1,7 @@
 
 for i in $(\grep "Vibrio-alginolyticus" Kraken2_Results_Dec2019.csv |grep "NO_" | awk -F ',' '{print $2}')
 do
-echo "Phylogenetics_Tree/"$i
-cp "Phylogenetics_Tree/"$i"*"  Phylogenetics_Tree/V_Algino/
+  echo "Phylogenetics_Tree/"$i
+  cp "Phylogenetics_Tree/"$i"*"  Phylogenetics_Tree/V_Algino/
 done
 
